66ca7c46d31536e432f5d758a05b33a9a9008861
mark-was <mark.waskelis@proton.me>
2025-03-31 21:35:57 -0400

    add args

--- consumer.py
+++ consumer.py
@@ -1,11 +1,24 @@
+import argparse
 from confluent_kafka import Consumer, TopicPartition
 
 BROKER = '127.0.0.1:9092'
 TOPIC = 'events'
+PARTITION = 0
+
+#argparses
+parser = argparse.ArgumentParser(description='Consume events from Kafka')
+parser.add_argument('-p', '--partition', help='partition to consume from', default=PARTITION)
+parser.add_argument('-t', '--topic', help='topic to consume from', default=TOPIC)
+args = parser.parse_args()
+
+PARTITION = int(args.partition)
+TOPIC = args.topic
 
 consumer = Consumer({'bootstrap.servers':BROKER, 'group.id':'events_group', 'enable.auto.commit':False, 'auto.offset.reset':'earliest'})
 
-partition = TopicPartition(TOPIC, 0)
+
+partition = TopicPartition(TOPIC, PARTITION)
+
 consumer.assign([partition])
 
 while True:
@@ -20,4 +33,3 @@ while True:
 
     print(f'Received message: {msg.value().decode("utf-8")}')
     consumer.commit()
-    
